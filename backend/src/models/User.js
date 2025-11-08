301bb25ad5b5201b91652c34a83856c1b68c4b91
Abhishek Shahi <shahiabhi9900@gmail.com>
2025-11-08 22:37:55 +0530

    auth sync with inngest

---
+++ backend/src/models/User.js
@@ -0,0 +1,25 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    require: true,
+    unique: true,
+  },
+  profileImage:{
+    type:String,
+    default:""
+  },
+  clerkId:{//this we are using to link clerk user with mongodb user
+        type: String,
+    require: true,
+    unique: true,
+  }
+},{timestamps:true});
+
+const User = mongoose.model("User", userSchema);
+export default User;
